b648878fbed302eae7621fa07fb83c70e9aea551
praivesi <praivesi@gmail.com>
2022-10-11 22:56:32 +0900

    WIP: ADD programmers/level2/Matrix.py

---
+++ programmers/level2/Matrix.py
@@ -0,0 +1,10 @@
+def solution(arr1, arr2):
+    answer = [[]]
+
+    for row in range(arr1):
+        num = 0
+
+        for col in range(arr1[row]):
+            num += arr1[row][col] * arr2[] 
+
+    return answer
