70ab3299aa92b56153d34baff3d729e538cb318d
praivesi <praivesi@gmail.com>
2022-10-12 15:07:11 +0900

    UPDATE programmers/level2/JumbOrTeleport.py

--- programmers/level2/JumpOrTeleport.py
+++ programmers/level2/JumpOrTeleport.py
@@ -1,14 +1,17 @@
-def solution(n):
-    answer = 0
+# def solution(n):
+#     answer = 0
 
-    while n > 0:
-        if n % 2 == 0:
-            n /= 2
-        else:
-            n -= 1
-            answer += 1
+#     while n > 0:
+#         if n % 2 == 0:
+#             n /= 2
+#         else:
+#             n -= 1
+#             answer += 1
     
-    return answer
+#     return answer
+
+def solution(n):
+    return bin(n).count('1')
 
 ans = solution(5)
 print('ans => ' + f'{ans}')
