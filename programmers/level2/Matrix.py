7efd14b7eb668839fe028eab1474c41d0add235d
praivesi <praivesi@gmail.com>
2022-10-12 14:04:24 +0900

    ADD programmers/level2/Matrix.py

--- programmers/level2/Matrix.py
+++ programmers/level2/Matrix.py
@@ -1,10 +1,23 @@
 def solution(arr1, arr2):
-    answer = [[]]
+    answer = []
 
-    for row in range(arr1):
-        num = 0
+    for row1 in range(len(arr1)):
+        row_ans = []
 
-        for col in range(arr1[row]):
-            num += arr1[row][col] * arr2[] 
+        for col2 in range(len(arr2[0])):
+            num = 0
+
+            for col1 in range(len(arr1[0])):
+                num += arr1[row1][col1] * arr2[col1][col2]                
+
+            row_ans.append(num)
+
+        answer.append(row_ans)
 
     return answer
+
+arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+
+ans = solution(arr1, arr2)
+print('ans => ' + f'{ans}')
