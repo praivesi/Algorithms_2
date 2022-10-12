b809dc81219efd52879bd0bcc311e67647cb6da8
praivesi <praivesi@gmail.com>
2022-10-12 15:05:06 +0900

    ADD programmers/level2/JumpOrTeleport.py

--- programmers/level2/JumpOrTeleport.py
+++ programmers/level2/JumpOrTeleport.py
@@ -1,6 +1,14 @@
 def solution(n):
-    batteries = []
+    answer = 0
 
+    while n > 0:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n -= 1
+            answer += 1
+    
+    return answer
 
 ans = solution(5)
 print('ans => ' + f'{ans}')
