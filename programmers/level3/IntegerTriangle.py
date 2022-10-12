e78d2a0d52f819acb5b4d85e71f5af7b967a59b4
praivesi <praivesi@gmail.com>
2022-10-12 18:07:08 +0900

    ADD programmers/level3/IntegerTriangle.py

--- programmers/level3/IntegerTriangle.py
+++ programmers/level3/IntegerTriangle.py
@@ -1,28 +1,32 @@
-tree = []
-dp = []
+# tree = []
+# dp = []
 
-def get_max(depth, width):
-    global tree, dp
+# def get_max(depth, width):
+#     global tree, dp
 
-    if depth >= len(tree) or width >= len(tree[depth]): return 0
-    if dp[depth][width] != -1: return dp[depth][width]
+#     if depth >= len(tree) or width >= len(tree[depth]): return 0
+#     if dp[depth][width] != -1: return dp[depth][width]
 
-    dp[depth][width] = tree[depth][width] + max(get_max(depth + 1, width), get_max(depth + 1, width + 1))
+#     dp[depth][width] = tree[depth][width] + max(get_max(depth + 1, width), get_max(depth + 1, width + 1))
 
-    return dp[depth][width]
+#     return dp[depth][width]
 
-def solution(triangle):
-    global tree, dp
+# def solution(triangle):
+#     global tree, dp
 
-    tree = triangle
+#     tree = triangle
 
-    for i in range(len(triangle)):
-        leaves = []
-        for j in range(len(triangle[i])):
-            leaves.append(-1)
-        dp.append(leaves)
+#     for i in range(len(triangle)):
+#         leaves = []
+#         for j in range(len(triangle[i])):
+#             leaves.append(-1)
+#         dp.append(leaves)
 
-    return get_max(0, 0)
+#     return get_max(0, 0)
+
+
+solution = lambda t, l = []: max(l) if not t else solution(t[1:], [x + max(y, z) for x,y,z in zip(t[0], [0] + l, l + [0])])
+
 
 triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
 ans = solution(triangle)
