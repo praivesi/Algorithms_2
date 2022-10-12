1723149d06d1243266ad58cb002abfe42e116e9b
praivesi <praivesi@gmail.com>
2022-10-12 14:30:16 +0900

    UPDATE programmers/leleve2/Matrix.py

--- programmers/level2/Matrix.py
+++ programmers/level2/Matrix.py
@@ -1,20 +1,23 @@
+# def solution(arr1, arr2):
+#     answer = []
+
+#     for row1 in range(len(arr1)):
+#         row_ans = []
+
+#         for col2 in range(len(arr2[0])):
+#             num = 0
+
+#             for col1 in range(len(arr1[0])):
+#                 num += arr1[row1][col1] * arr2[col1][col2]                
+
+#             row_ans.append(num)
+
+#         answer.append(row_ans)
+
+#     return answer
+
 def solution(arr1, arr2):
-    answer = []
-
-    for row1 in range(len(arr1)):
-        row_ans = []
-
-        for col2 in range(len(arr2[0])):
-            num = 0
-
-            for col1 in range(len(arr1[0])):
-                num += arr1[row1][col1] * arr2[col1][col2]                
-
-            row_ans.append(num)
-
-        answer.append(row_ans)
-
-    return answer
+    return [[sum(a*b for a, b in zip(A_row, B_col)) for B_col in zip(*arr2)] for A_row in arr1]
 
 arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
 arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
