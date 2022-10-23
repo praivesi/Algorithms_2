1f04d86c06721778518b398adcc117b5e711b988
praivesi <praivesi@gmail.com>
2022-10-23 13:07:22 +0900

    WIP2: programmers/level2/StockPrice.py

--- programmers/level2/StockPrice.py
+++ programmers/level2/StockPrice.py
@@ -1,12 +1,18 @@
 def solution(prices):
-    answer = []
+    answer = [0 for _ in range(len(prices))]
     
-    up = [[] for _ in range(10000)]
+    up = [[]] + [[] for _ in range(10000)] + [[]]
+    
 
     for i, p in enumerate(prices):
-        up[p] = i
+        up[p].append(i)
 
-    
-
+        if up[p + 1]:
+            for pi in up[p + 1]:
+                answer[pi] = i - pi
 
     return answer
+
+prices = [1, 2, 3, 2, 3]
+ans = solution(prices)
+print(ans)
