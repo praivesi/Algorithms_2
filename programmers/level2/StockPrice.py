07bf8dc83ac8cb1ba0202b41613e47b2d7e9b888
hsoh <hsoh@ivstech.co.kr>
2022-10-24 18:57:49 +0900

    WIP: programmers/level2/StockPrice.py

--- programmers/level2/StockPrice.py
+++ programmers/level2/StockPrice.py
@@ -3,16 +3,28 @@ def solution(prices):
     
     up = [[]] + [[] for _ in range(10000)] + [[]]
     
-
+    max_p = 0
     for i, p in enumerate(prices):
+        max_p = max(p, max_p)
         up[p].append(i)
 
-        if up[p + 1]:
-            for pi in up[p + 1]:
+        for j in range(p + 1, max_p + 1):
+            if not up[j]: continue
+            
+            for pi in up[j]:
                 answer[pi] = i - pi
+                
+            up[j] = []
 
+    for upp in up:
+        if not upp: continue
+
+        for ix, ui in enumerate(upp):
+            answer[ui] = len(prices) - 1 - ui
+            
     return answer
 
-prices = [1, 2, 3, 2, 3]
+
+prices =  [2, 3, 1, 2, 3]
 ans = solution(prices)
 print(ans)
