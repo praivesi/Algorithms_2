285a81c713464e3d4b8fae93a3c3f4b20de3574b
praivesi <praivesi@gmail.com>
2022-10-25 23:28:55 +0900

    WIP: add programmers/level2/Chat.py

---
+++ programmers/level2/Chat.py
@@ -0,0 +1,33 @@
+def solution(record):
+    answer = []
+
+    nic_d = {}
+    change_d = {}
+    log = []
+
+    for r in record:
+        split = r.split(' ')
+
+        if split[0] == 'Enter':
+            log.append(['들어왔습니다.', split[1]])
+        elif split[0] == 'Leave':
+            log.append(['나갔습니다.', split[1]])
+            continue
+        else: # Change
+            change_d[split[1]] = split[2]
+            continue
+            
+        nic_d[split[1]] = split[2]
+
+    for cd in change_d:
+        nic_d[cd[0]] = cd[1]
+
+    for l in log:
+        msg = nic_d[l[1]] + '님이 ' + l[0]
+        answer.append(msg)
+
+    return answer
+
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+ans = solution(record)
+print(ans)
