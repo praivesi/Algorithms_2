7029659ebf364fcf1cdd39e661343a3ab5c47f20
praivesi <praivesi@gmail.com>
2022-10-25 21:44:56 +0900

    ADD programmers/level2/StockPrice.py

--- programmers/level2/StockPrice.py
+++ programmers/level2/StockPrice.py
@@ -1,29 +1,32 @@
 def solution(prices):
     answer = [0 for _ in range(len(prices))]
-    
-    up = [[]] + [[] for _ in range(10000)] + [[]]
-    
-    max_p = 0
+
+    stack = []
+
     for i, p in enumerate(prices):
-        max_p = max(p, max_p)
-        up[p].append(i)
+        if not stack:
+            stack.append([i, p])
+            continue
+        
+        if stack[-1][1] <= p: 
+            stack.append([i, p])
+            continue
 
-        for j in range(p + 1, max_p + 1):
-            if not up[j]: continue
+        while stack:
+            if stack[-1][1] <= p: break
+
+            top = stack.pop(-1)
+            answer[top[0]] = i - top[0]
+
+        stack.append([i, p])
             
-            for pi in up[j]:
-                answer[pi] = i - pi
-                
-            up[j] = []
+    while stack:
+        top = stack.pop(-1)
 
-    for upp in up:
-        if not upp: continue
-
-        for ix, ui in enumerate(upp):
-            answer[ui] = len(prices) - 1 - ui
+        answer[top[0]] = len(prices) - 1 - top[0]
             
     return answer
 
-prices =  [2, 3, 1, 2, 3]
+prices =  [1, 2, 3, 2, 3]
 ans = solution(prices)
 print(ans)
