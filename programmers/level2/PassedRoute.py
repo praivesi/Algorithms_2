c16741a42a9979f16849d83585c556a07fb37410
praivesi <praivesi@gmail.com>
2022-11-02 18:17:47 +0900

    WIP: programmers/level2/PassedRoute.py

--- programmers/level2/PassedRoute.py
+++ programmers/level2/PassedRoute.py
@@ -15,6 +15,13 @@ def solution(dirs):
     i = 0
     for d in list(dirs):
         print("f'{i}' th step >>> " + f'{d}' + ', x: ' + f'{pos[0]}' + ', y: ' + f'{pos[1]}')
+
+        print('visited[0][0][1][0] => ' + f'{visited[0][0][1][0]}')
+        print('visited[1][0][2][0] => ' + f'{visited[0][0][1][0]}')
+        print('visited[2][0][3][0] => ' + f'{visited[0][0][1][0]}')
+        print('visited[3][0][4][0] => ' + f'{visited[0][0][1][0]}')
+        print('visited[4][0][5][0] => ' + f'{visited[0][0][1][0]}')
+
         if d == 'L':
             if pos[0] == -5: continue
             if not visited[pos[0]][pos[1]][pos[0] - 1][pos[1]]:
@@ -28,11 +35,17 @@ def solution(dirs):
             if pos[0] == 5: continue
             print('TF => ' + f'{pos[0]}' + ', ' + f'{pos[1]}' + ', ' + f'{pos[0] + 1}' + ', ' + f'{pos[1]}')
             print('TF => ' + f'{visited[pos[0]][pos[1]][pos[0] + 1][pos[1]]}')
+            print('visited[pos[0]] => ' + f'{visited[pos[0]]}')
+            print('visited[pos[0]][pos[1]] => ' + f'{visited[pos[0]][pos[1]]}')
+            print('visited[pos[0]][pos[1]][pos[0] + 1] => ' + f'{visited[pos[0]][pos[1]][pos[0] + 1]}')
+            print('visited[pos[0]][pos[1]][pos[0] + 1][pos[1]] => ' + f'{visited[pos[0]][pos[1]][pos[0] + 1][pos[1]]}')
             if not visited[pos[0]][pos[1]][pos[0] + 1][pos[1]]:
                 print("True invoked >>> " + ', x: ' + f'{pos[0]}' + ', y: ' + f'{pos[1]}' + ', next x: ' + f'{pos[0] + 1}' + ', next y: ' + f'{pos[1]}') 
+                # visited[pos[0]][pos[1]][pos[0] + 1][pos[1]] = True
                 visited[pos[0]][pos[1]][pos[0] + 1][pos[1]] = True
                 visited[pos[0] + 1][pos[1]][pos[0]][pos[1]] = True
                 print(f'{i}' + 'th R visited.')
+                
                 routeCnt += 1
             pos[0] += 1
 
