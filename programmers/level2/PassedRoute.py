5c0784a9fcc16b504452cb1aada1f67af5c1cd94
praivesi <praivesi@gmail.com>
2022-11-03 23:41:34 +0900

    ADD programmers/level2/PasseRoute.py

--- programmers/level2/PassedRoute.py
+++ programmers/level2/PassedRoute.py
@@ -1,76 +1,25 @@
 def solution(dirs):
-    visited = []
-    for i in range(6):
-        visited.append([])
-        for j in range(6):
-            visited[i].append([])
-            for k in range(6):
-                visited[i][j].append([])
-                for _ in range(6):
-                    visited[i][j][k].append(False)
+    answer = 0
+    x = 5
+    y = 5
+    map = []
+    move = {'U':(0,1), 'D':(0,-1), 'L':(-1,0), 'R':(1,0)}
+
+    for i in range(len(dirs)):
+        (dy, dx) = move[dirs[i]]
+        if not (0 <= x + dx and x + dx <= 10 and 0 <= y + dx and y + dx <= 10):
+            continue
+        map.append((x, y, x + dx, y + dy))
+        map.append((x + dx, y + dy, x, y))
+        x = x + dx
+        y = y + dy
     
-    routeCnt = 0
-    pos = [0, 0]
+    mapset = set(map)
+    answer = len(mapset) // 2
 
-    i = 0
-    for d in list(dirs):
-        print("f'{i}' th step >>> " + f'{d}' + ', x: ' + f'{pos[0]}' + ', y: ' + f'{pos[1]}')
+    return answer
 
-        print('visited[0][0][1][0] => ' + f'{visited[0][0][1][0]}')
-        print('visited[1][0][2][0] => ' + f'{visited[0][0][1][0]}')
-        print('visited[2][0][3][0] => ' + f'{visited[0][0][1][0]}')
-        print('visited[3][0][4][0] => ' + f'{visited[0][0][1][0]}')
-        print('visited[4][0][5][0] => ' + f'{visited[0][0][1][0]}')
 
-        if d == 'L':
-            if pos[0] == -5: continue
-            if not visited[pos[0]][pos[1]][pos[0] - 1][pos[1]]:
-                visited[pos[0]][pos[1]][pos[0] - 1][pos[1]] = True
-                visited[pos[0] - 1][pos[1]][pos[0]][pos[1]] = True
-                print(f'{i}' + 'th L visited.')
-                routeCnt += 1
-            pos[0] -= 1
-
-        elif d == 'R':
-            if pos[0] == 5: continue
-            print('TF => ' + f'{pos[0]}' + ', ' + f'{pos[1]}' + ', ' + f'{pos[0] + 1}' + ', ' + f'{pos[1]}')
-            print('TF => ' + f'{visited[pos[0]][pos[1]][pos[0] + 1][pos[1]]}')
-            print('visited[pos[0]] => ' + f'{visited[pos[0]]}')
-            print('visited[pos[0]][pos[1]] => ' + f'{visited[pos[0]][pos[1]]}')
-            print('visited[pos[0]][pos[1]][pos[0] + 1] => ' + f'{visited[pos[0]][pos[1]][pos[0] + 1]}')
-            print('visited[pos[0]][pos[1]][pos[0] + 1][pos[1]] => ' + f'{visited[pos[0]][pos[1]][pos[0] + 1][pos[1]]}')
-            if not visited[pos[0]][pos[1]][pos[0] + 1][pos[1]]:
-                print("True invoked >>> " + ', x: ' + f'{pos[0]}' + ', y: ' + f'{pos[1]}' + ', next x: ' + f'{pos[0] + 1}' + ', next y: ' + f'{pos[1]}') 
-                # visited[pos[0]][pos[1]][pos[0] + 1][pos[1]] = True
-                visited[pos[0]][pos[1]][pos[0] + 1][pos[1]] = True
-                visited[pos[0] + 1][pos[1]][pos[0]][pos[1]] = True
-                print(f'{i}' + 'th R visited.')
-                
-                routeCnt += 1
-            pos[0] += 1
-
-        elif d == 'U':
-            if pos[1] == 5: continue
-            if not visited[pos[0]][pos[1]][pos[0]][pos[1] + 1]:
-                visited[pos[0]][pos[1]][pos[0]][pos[1] + 1] = True
-                visited[pos[0]][pos[1] + 1][pos[0]][pos[1]] = True
-                print(f'{i}' + 'th U visited.')
-                routeCnt += 1
-            pos[1] += 1
-
-        else: # 'D'
-            if pos[1] == -5: continue
-            if not visited[pos[0]][pos[1]][pos[0]][pos[1] - 1]:
-                visited[pos[0]][pos[1]][pos[0]][pos[1] - 1] = True
-                visited[pos[0]][pos[1] - 1][pos[0]][pos[1]] = True
-                print(f'{i}' + 'th D visited.')
-                routeCnt += 1
-            pos[1] -= 1
-        
-        i += 1
-
-    return routeCnt
-
-dirs = "LLLLLLLLLLRRRRRRRRRR"
+dirs = "ULURRDLLU"
 ans = solution(dirs)
 print(ans)
