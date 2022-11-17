7b225d93613f7ed5b453ad4c33363fed68f4b5ee
praivesi <praivesi@gmail.com>
2022-11-17 17:18:43 +0900

    UPDATE programmers/level1/2016.py

--- programmers/level1/2016.py
+++ programmers/level1/2016.py
@@ -1,21 +1,25 @@
 def solution(a, b):
-    past = 1
+    past = 0
     
     if a != 1:
-        if a < 3: past += 31
-        if a < 4: past += 29
-        if a < 5: past += 31
-        if a < 6: past += 30
-        if a < 7: past += 31
-        if a < 8: past += 30
-        if a < 9: past += 31
-        if a < 10: past += 31
-        if a < 11: past += 30
-        if a < 12: past += 31
-        if a < 13: past += 30
+        if a > 1: past += 31
+        if a > 2: past += 29
+        if a > 3: past += 31
+        if a > 4: past += 30
+        if a > 5: past += 31
+        if a > 6: past += 30
+        if a > 7: past += 31
+        if a > 8: past += 31
+        if a > 9: past += 30
+        if a > 10: past += 31
+        if a > 11: past += 30
     
+    b -= 1
     past += b
 
-    cal = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    cal = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
     
-    return cal[(4 + past) % 7]
+    return cal[past % 7]
+
+ans = solution(5, 24)
+print(ans)
